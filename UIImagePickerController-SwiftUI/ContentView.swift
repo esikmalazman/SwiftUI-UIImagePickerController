d6d5792a7d726619c7cbbc77144e63f611f0b622
Ikmal Azman <opikmalazman@gmail.com>
2023-02-21 02:19:00 +0000

    create & implement basic design to use UIImagePickerController wrapper in SwiftUI View

--- UIImagePickerController-SwiftUI/ContentView.swift
+++ UIImagePickerController-SwiftUI/ContentView.swift
@@ -3,19 +3,45 @@
 //  UIImagePickerController-SwiftUI
 //
 //  Created by Ikmal Azman on 21/02/2023.
-//
+// https://www.appcoda.com/swiftui-camera-photo-library/
 
 import SwiftUI
 
 struct ContentView: View {
+    /// `isShowPhotoLibrary`: variable to control visibility of the photo library
+    @State var isShowPhotoLibrary = false
+    
+    /// ` image`: variable to store selected image
+    @State var image = UIImage()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image(uiImage: image)
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .edgesIgnoringSafeArea(.all)
+            
+            Button {
+                /// Toggle the state of `isShowPhotoLibrary`
+                self.isShowPhotoLibrary.toggle()
+            } label: {
+                Label("Photo Library", systemImage: "photo")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
+        /// in `iOS 14 and above`, we should user PHViewController to access photo library.
+        /// .sheet(): modifier to present a sheet(Present Modally)
+        /// isPresented: Binding boolean value to determin if the sheet should be presented or else
+        .sheet(isPresented: $isShowPhotoLibrary) {
+            /// Present the image picker with access to Photo Library. To access Camera, we need to change the sourceType to `.camera` and add the `Privacy Usage Description`
+            ImagePicker(selectedImage: $image, sourceType: .photoLibrary)
+            
+            // ImagePicker(selectedImage: $image, sourceType: .camera)
+        }
     }
 }
 
