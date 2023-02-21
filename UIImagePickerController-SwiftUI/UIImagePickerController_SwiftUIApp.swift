22bcb8de04cc41aa78c040eccb0f6bcf35d19d3b
Ikmal Azman <opikmalazman@gmail.com>
2023-02-21 01:24:27 +0000

    Initial Commit

---
+++ UIImagePickerController-SwiftUI/UIImagePickerController_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIImagePickerController_SwiftUIApp.swift
+//  UIImagePickerController-SwiftUI
+//
+//  Created by Ikmal Azman on 21/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct UIImagePickerController_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
